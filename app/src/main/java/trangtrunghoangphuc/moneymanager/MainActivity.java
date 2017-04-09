f36dd499a7f4b8601c914791f0228effb77801bd
Trang Trung Hoàng Phúc <trangtrunghoangphuc@gmail.com>
2017-04-09 11:10:57 +0700

    Testing

--- app/src/main/java/trangtrunghoangphuc/moneymanager/MainActivity.java
+++ app/src/main/java/trangtrunghoangphuc/moneymanager/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     EditText txtTien;
     ImageButton btnDatePicker;
     Button btnThayDoiActiviy,btnThemTien,btnThongKe;
+    EditText Test;
     final int requestCodeActivity=1;
     final int thayDoiTaiKhoanResultCode=3;
     Calendar cal=Calendar.getInstance();
